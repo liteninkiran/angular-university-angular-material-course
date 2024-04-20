b42c9e29d6a72b99f8084646cd9c0587187950fd
Kiran Anand <liteninkiran@hotmail.com>
2024-04-20 12:56:47 +0100

    008 - Angular Material Tab Groups (Clean Up)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,36 +1,30 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {Observable} from "rxjs";
-import {CoursesService} from "../services/courses.service";
-import {map} from "rxjs/operators";
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { Observable } from 'rxjs';
+import { CoursesService } from '../services/courses.service';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+    styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
 
-    beginnerCourses$: Observable<Course[]>;
-
-    advancedCourses$: Observable<Course[]>;
+    public beginnerCourses$: Observable<Course[]>;
+    public advancedCourses$: Observable<Course[]>;
 
     constructor(private coursesService: CoursesService) {
 
     }
 
-    ngOnInit() {
-
+    public ngOnInit(): void {
         const courses$ = this.coursesService.findAllCourses();
-
         this.beginnerCourses$ = courses$.pipe(
-          map(courses => courses.filter(course => course.category === 'BEGINNER') )
+            map(courses => courses.filter(course => course.category === 'BEGINNER'))
         );
-
         this.advancedCourses$ = courses$.pipe(
-            map(courses => courses.filter(course => course.category === 'ADVANCED') )
+            map(courses => courses.filter(course => course.category === 'ADVANCED'))
         );
-
     }
-
 }
